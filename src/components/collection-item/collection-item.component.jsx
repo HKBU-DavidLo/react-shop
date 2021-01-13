084e34154361d268185a814b9c71cc1cf6bffa6a
David Lo <19451415@life.hkbu.edu.hk>
2021-01-14 02:22:44 +0800

    form components

--- src/components/collection-item/collection-item.component.jsx
+++ src/components/collection-item/collection-item.component.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import CollectionPreview from '../collection-preview/collection-preview.component';
 
 import './collection-item.styles.scss'
 
@@ -7,11 +6,10 @@ const CollectionItem = ({ id, name, price, imageUrl }) => (
     <div className="collection-item">
         <div className="image"
             style={{backgroundImage: `url(${imageUrl})`
-        }}>
-           <div className="collection-footer">
-                <span className="name">{ name }</span>
-                <span className="price">{ price }</span>
-           </div>
+        }} />
+        <div className="collection-footer">
+            <span className="name">{ name }</span>
+            <span className="price">{ price }</span>
         </div>
     </div>
 )

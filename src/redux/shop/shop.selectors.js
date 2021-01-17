6d5e918f6ba074c8cde3ef03ad62a24a0541e7ee
David Lo <19451415@life.hkbu.edu.hk>
2021-01-18 01:56:43 +0800

    collection

---
+++ src/redux/shop/shop.selectors.js
@@ -0,0 +1,27 @@
+import { createSelector } from 'reselect'
+import memoize from 'lodash.memoize'
+import collectionItemComponent from '../../components/collection-item/collection-item.component'
+
+const COLLECTION_ID_MAP = {
+    hats: 1,
+    sneakers: 2,
+    jackets: 3,
+    womens: 4,
+    mens: 5
+}
+
+const selectShop = state => state.shop
+
+export const selectCollections = createSelector(
+    [selectShop],
+    shop => shop.collections
+)
+
+export const selectCollection = memoize((collectionUrlParams) =>
+    createSelector(
+        [selectCollections],
+        collections => 
+            collections.find(
+                collection => collection.id === COLLECTION_ID_MAP[collectionUrlParams])
+    )
+)
